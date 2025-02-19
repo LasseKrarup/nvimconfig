c474a9257e5de0ad443f9c9ea7892207bcc4adf1
Lasse Krarup <latze95@gmail.com>
2025-02-19 11:49:41 +0100

    Format bitbake files with 2 spaces

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -6,3 +6,13 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+-- BitBake specific settings
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "bitbake",
+  callback = function()
+    vim.bo.shiftwidth = 2
+    vim.bo.tabstop = 2
+    vim.bo.expandtab = true
+  end,
+})
