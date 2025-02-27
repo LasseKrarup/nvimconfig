ed1553b3fe2e52eb31f7738df62f7ad75bb36b9c
Lasse Krarup <latze95@gmail.com>
2025-02-27 10:44:49 +0100

    Copy lines up/down keymap

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,9 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Copy lines up/down
+vim.keymap.set("n", "<S-A-j>", "yyp", { noremap = true, desc = "Copy line down" })
+vim.keymap.set("i", "<S-A-j>", "<Esc>yypi", { noremap = true, desc = "Copy line down" })
+vim.keymap.set("n", "<S-A-k>", "yyP", { noremap = true, desc = "Copy line up" })
+vim.keymap.set("i", "<S-A-k>", "<Esc>yyPi", { noremap = true, desc = "Copy line up" })
