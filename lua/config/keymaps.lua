c82ce7623548568b0e6e89d4f77c8359f6d65009
Lasse Krarup <latze95@gmail.com>
2025-03-10 09:58:48 +0100

    Add copy selection keymaps in visual mode

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -5,5 +5,7 @@
 -- Copy lines up/down
 vim.keymap.set("n", "<S-A-j>", "yyp", { noremap = true, desc = "Copy line down" })
 vim.keymap.set("i", "<S-A-j>", "<Esc>yypi", { noremap = true, desc = "Copy line down" })
+vim.keymap.set("v", "<S-A-j>", "yp", { noremap = true, desc = "Copy selection down" })
 vim.keymap.set("n", "<S-A-k>", "yyP", { noremap = true, desc = "Copy line up" })
+vim.keymap.set("v", "<S-A-k>", "yP", { noremap = true, desc = "Copy selection up" })
 vim.keymap.set("i", "<S-A-k>", "<Esc>yyPi", { noremap = true, desc = "Copy line up" })
